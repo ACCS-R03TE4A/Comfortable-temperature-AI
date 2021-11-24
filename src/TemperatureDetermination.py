3204eeac8d6e1cd17b20c91485567e867e575410
Naoki Sasaki <tejc18074@gmail.com>
2021-11-24 12:48:23 +0900

    テスト作成

--- src/TemperatureDetermination.py
+++ src/TemperatureDetermination.py
@@ -1,21 +1,29 @@
 class TemperatureDetermination:
-    input_temperature = -1.0  #入力温度
+    '''
+    入力温度感覚、温度感覚カテゴリはAI実装後は削除する。
+    デフォルトの値は現在は仮の値とする。
+    ACCS-SERVERより外部気温が入力される。
+    '''
+    input_temperature = 25.0  #入力温度
     input_temperature_sense = "3"   #入力温度感覚（1~5の値）
-    output_temperature = -1.0 #出力温度
+    output_temperature = 25.0 #出力温度
+    SENS_CATEGORY = {"1":-5.0, "2":-3.0, "3":0.0, "4":2.0, "5":5.0} #温度感覚カテゴリ
 
-    def __init__(self,input_temperature,temperature_sense):
+    def __init__(self,input_temperature,input_temperature_sense):
         try:
             if((input_temperature < 5) | (input_temperature > 50)):
                 raise ValueError()
             self.input_temperature = float(input_temperature)
-            self.temperature_sense = str(temperature_sense)
+            self.input_temperature_sense = str(input_temperature_sense)
         except ValueError:
+            self.input_temperature = 25.0
             print("Temperature is out of range.")
-            
-    
+
+               
     def decision_base(self):
-        SENS_CATEGORY = {"1":-5.0, "2":-3.0, "3":0.0, "4":2.0, "5":5.0}
         try:
-            self.output_temperature = self.input_temperature + SENS_CATEGORY[self.input_temperature_sense]
+            output = self.input_temperature + self.SENS_CATEGORY[self.input_temperature_sense]
+            return output
         except KeyError:
+            self.input_temperature_sense = "3"
             print("No such temperature sense.")
--- tests/test_TemperatureDetermination.py
+++ tests/test_TemperatureDetermination.py
@@ -1,1 +1,32 @@
-from src.TemperatureDetermination import decision_base
+from src.TemperatureDetermination import TemperatureDetermination
+import pytest
+
+def test_init_temperature():
+    td = TemperatureDetermination(20.0,1)
+    assert td.input_temperature == 20.0
+
+def test_init_temperature_NG_sub():
+    #範囲外の温度感覚が入力された場合デフォルト値の25.0になる
+    td2 = TemperatureDetermination(-100.0,1)
+    assert td2.input_temperature == 25.0
+
+def test_init_temperature_NG_upper():
+    td2 = TemperatureDetermination(100.0,1)
+    assert td2.input_temperature == 25.0
+
+def test_init_sense():
+    td3 = TemperatureDetermination(25.0,1)
+    td3.decision_base()
+    assert td3.input_temperature_sense == "1"
+
+def test_init_sense_NG():
+    #範囲外の温度感覚が入力された場合デフォルト値の３になる
+    td4 = TemperatureDetermination(25.0,10)
+    td4.decision_base()
+    assert td4.input_temperature_sense == "3"
+
+def test_decision_base():
+    td5 = TemperatureDetermination(20.0,1)
+    td5.decision_base()
+    assert td5.decision_base() == 15.0
+
