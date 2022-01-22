5de539f77fc65495002b146c8895060747c14bb4
te180453 <te180453jc21@gmail.com>
2022-01-22 13:03:34 +0900

    定期学習スクリプトの作成

---
+++ scripts/learn_and_save.py
@@ -0,0 +1,14 @@
+import os
+import json
+import sys
+sys.path.append("/home/user/ACCS/ACCS-SERVER")
+from Comfortable_temperature_AI.src.TemperatureDetermination import ComfortTemperaturePredictionAI
+from logging import getLogger, config, basicConfig, DEBUG
+logger = getLogger(__name__)
+with open("log_config.json", "r") as f:
+    config.dictConfig(json.load(f))
+basicConfig(level=DEBUG)
+
+ai = ComfortTemperaturePredictionAI()
+ai.create_model()
+logger.debug(f"終了:{__name__}")
---
+++ scripts/learn_and_save.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -z $ACCS_HOME ]; then
+  echo 'Please register ACCS_HOME as an environment variable first.ACCS_HOME is the absolute path of ACCS-SERVER.' >&2
+  exit -1
+fi
+echo "The path to ACCS-SERVER is $ACCS_HOME."
+cd $ACCS_HOME
+source env/bin/activate
+rm Learn.csv
+python Comfortable_temperature_AI/scripts/learn_and_save.py
+echo "save model."
